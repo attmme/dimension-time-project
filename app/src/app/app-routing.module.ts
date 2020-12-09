eba23d6ae7bc2c8bfce1fa6817a655f1a7407aba
victorgg1993 <vigomga1993@gmail.com>
2020-12-09 14:33:21 +0100

    empezado routing básico

--- app/src/app/app-routing.module.ts
+++ app/src/app/app-routing.module.ts
@@ -1,10 +1,22 @@
+import { UserComponent } from './modules/users/pages/user/user.component';
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './modules/home/components/home/home.component';
+import { LoginFormComponent } from './modules/login/components/login-form/login-form.component';
+import { RegisterFormComponent } from './modules/users/components/register-form/register-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: 'home', component: HomeComponent },
+  { path: 'login', component: LoginFormComponent },
+  { path: 'register', component: RegisterFormComponent },
+  { path: 'tasks', component: UserComponent },
+
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: '**', component: HomeComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
-  exports: [RouterModule]
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}
