8e2964058dc12533be2ea055885618135bd095e3
Oscar <ogomezsts51l@gmail.com>
2020-12-10 20:16:09 +0100

    registre

--- app/src/app/modules/register/register.component.ts
+++ app/src/app/modules/register/register.component.ts
@@ -2,8 +2,6 @@ import { Router } from '@angular/router';
 import { Component, EventEmitter, Input, OnInit, Output } from '@angular/core';
 import { FormControl, FormGroup } from '@angular/forms';
 import { FirebaseService } from 'src/app/shared/services/firebase.service';
-import { AngularFireAuth } from '@angular/fire/auth';
-
 
 @Component({
   selector: 'app-register',
@@ -14,13 +12,21 @@ export class RegisterComponent implements OnInit {
   @Input() error: string | null;
   @Output() submitEM = new EventEmitter();
 
-  pare_asd: string;
+  ngOnInit(): void { }
 
   constructor(private trucazo_router: Router, private fbService: FirebaseService) {
-    this.pare_asd = 'test';
+    //this.pare_asd = 'test';
   }
 
-  ngOnInit(): void { }
+  // Formulari
+  pare_asd: string; // Fa falta?
+  usuari: string;
+  email: string;
+  pass1: string;
+  pass2: string;
+
+  // Validació camps
+  valid = false;
 
   form: FormGroup = new FormGroup({
     username: new FormControl(''),
@@ -29,12 +35,33 @@ export class RegisterComponent implements OnInit {
   });
 
   submit() {
-    if (this.form.valid) {
-      this.submitEM.emit(this.form.value);
-      this.fbService.registrar('pepito@pepito.com', '123456');
-    }
-
+    this.checkUser(); // Mirar com guardar user després
+    this.checkMail();
+    this.checkPass1();
+    this.checkPass2();
+    //if (this.form.valid) {
+    //this.submitEM.emit(this.form.value);
+    //this.fbService.registrar('pepito@pepito.com', '123456'); APAGAT
+    //}
     // trucazo
     // this.trucazo_router.navigateByUrl('/tasks');
   }
+
+  checkUser() {
+
+  }
+
+  checkMail() {
+
+  }
+
+  checkPass1() {
+
+  }
+
+  checkPass2() {
+
+  }
+
+
 }
