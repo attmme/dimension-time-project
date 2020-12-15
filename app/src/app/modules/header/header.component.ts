f6463d7ee3c7ceeaed28cd291302e2f97000719f
victorgg1993 <vigomga1993@gmail.com>
2020-12-16 00:20:13 +0100

    backup

--- app/src/app/modules/header/header.component.ts
+++ app/src/app/modules/header/header.component.ts
@@ -24,6 +24,7 @@ export class HeaderComponent implements OnInit {
   templatePersonal: TemplateRef<any>;
 
   llista = [];
+  tasques_trello = [];
   nomUsuari: string;
 
   constructor(
@@ -40,14 +41,55 @@ export class HeaderComponent implements OnInit {
     this.service.getUserName().then((data) => {
       this.nomUsuari = data.data()['user'];
     });
+
+    this.fbService.readColl('tasks').then((data) => {
+      data.map((el, id) =>
+        this.tasques_trello.push({ id: id, nom: el['nom'] })
+      );
+    });
   }
 
   isLogged = () => localStorage.getItem('userId');
 
   clickPersonal() {
+    let llista_reduida = [];
+    let llista_tmp = [];
+
     // refrescar la llista
-    this.agafarTasquesUsuariDB().then(
+    this.agafarTasquesUsuariDB().then(() => {
+      for (let i = 0; i < this.tasques_trello.length; i++) {
+        llista_reduida.push(
+          this.llista.filter((el) => el.titol == this.tasques_trello[i].nom)
+        );
+      }
+
+      for (let i = 0; i < llista_reduida.length; i++) {
+
+        let t = [
+          {
+            titol: llista_reduida[i][0].titol,
+            total: llista_reduida[i].reduce(
+              (accumulator, currentValue) => {
+                accumulator.horesMinutsSegons = this.adderHMS(
+                  accumulator.horesMinutsSegons,
+                  currentValue.horesMinutsSegons
+                );
+                return accumulator;
+              },
+              { horesMinutsSegons: '00:00:00' }
+            ).horesMinutsSegons,
+          },
+        ];
+
+        llista_tmp.push(t);
+      }
+
+      this.llista = [];
+      this.llista = [...llista_tmp];
+
+    }).then(
       ()=>{
+        console.table('fora: ', this.llista);
         this.modal.open(this.templatePersonal, { size: 'lg' });
       }
     );
@@ -58,7 +100,51 @@ export class HeaderComponent implements OnInit {
     let tempsFinal = +new Date(data_final);
     let seconds = Math.abs(tempsFinal - tempsInici);
     //console.log("PROVES: ", new Date(seconds * 1000).toISOString());
-    return new Date(seconds * 1000).toISOString().substr(11, 8);
+    let dada = new Date(seconds * 1000).toISOString().substr(11, 8);
+    return dada;
+  }
+
+  adderHMS(startTime, endTime) {
+    var times = [0, 0, 0];
+    var max = times.length;
+
+    var a = (startTime || '').split(':');
+    var b = (endTime || '').split(':');
+
+    // normalize time values
+    for (var i = 0; i < max; i++) {
+      a[i] = isNaN(parseInt(a[i])) ? 0 : parseInt(a[i]);
+      b[i] = isNaN(parseInt(b[i])) ? 0 : parseInt(b[i]);
+    }
+
+    // store time values
+    for (var i = 0; i < max; i++) {
+      times[i] = a[i] + b[i];
+    }
+
+    var hours = times[0];
+    var minutes = times[1];
+    var seconds = times[2];
+
+    if (seconds >= 60) {
+      var m = (seconds / 60) << 0;
+      minutes += m;
+      seconds -= 60 * m;
+    }
+
+    if (minutes >= 60) {
+      var h = (minutes / 60) << 0;
+      hours += h;
+      minutes -= 60 * h;
+    }
+
+    return (
+      ('0' + hours).slice(-2) +
+      ':' +
+      ('0' + minutes).slice(-2) +
+      ':' +
+      ('0' + seconds).slice(-2)
+    );
   }
 
   clickResumen() {
@@ -66,14 +152,12 @@ export class HeaderComponent implements OnInit {
   }
 
   agafarTasquesUsuariDB() {
-
     this.llista = [];
 
     return this.fbService
       .readColl(`users/${this.service.getToken()}/tasks`)
       .then((data) => {
-        data.map(element => {
-
+        data.map((element) => {
           let d_init = element['data_inici'].seconds;
           let d_final = element['data_final'].seconds;
           this.llista = [
